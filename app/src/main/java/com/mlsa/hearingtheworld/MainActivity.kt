3e60fa0d5a894ad46f5d13a2b78c69a200978468
Sijan <sijan@greatbear.tech>
2023-02-26 21:30:58 +0545

    initial commit

---
+++ app/src/main/java/com/mlsa/hearingtheworld/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mlsa.hearingtheworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
